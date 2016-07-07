459425617e0693036567c973fea6acb4c53086c8
sareid <andrew.reid@flatironschool.com>
2016-07-06 23:04:13 -0400

    Adds country show page and country comparison overview

--- app/components/country-stats/component.js
+++ app/components/country-stats/component.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  populationStyled: Ember.computed( function(){
+
+  populationStyled: Ember.computed('model.@each.state', function(){
     return this.get('model.population').toLocaleString();
   }),
-  numberStyled: Ember.computed( function(){
+  numberStyled: Ember.computed('model.@each.state', function(){
     return this.get('model.gdp_per_capita').toLocaleString();
   })
 });
